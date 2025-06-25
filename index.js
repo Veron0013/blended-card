79ece239df3fc3e867cd513adb44e3c9a139ce43
Veron0013 <iv_mirsoft@ukr.net>
2025-06-25 04:49:59 +0000

    Deploying to gh-pages from @ Veron0013/blended-card@09aabb41cc838606c13cdf888e33c3d642eb26bc 🚀

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-import"./assets/styles-BK7AYJoX.js";import{a as u}from"./assets/vendor-DGDcxXwr.js";const r={BASE_URL:"https://dummyjson.com/products",qCategories:"https://dummyjson.com/products/categories",_perPage:10,categoryList:document.querySelector(".categories"),productList:document.querySelector(".products")};function a(t,e,s){t.insertAdjacentHTML("beforeend",s(e))}const p=t=>t.map(({name:e,url:s})=>`<li class="categories__item" data-url="${s}">
-					<button  class="categories__btn" type="button">${e}
+import"./assets/styles-DT-UWGMR.js";import{a as M}from"./assets/vendor-DGDcxXwr.js";const t={BASE_URL:"https://dummyjson.com/products",qCategories:"https://dummyjson.com/products/categories",currentQuery:"",categoryList:document.querySelector(".categories"),productList:document.querySelector(".products"),load_more:document.querySelector(".load-more__btn"),productModal:document.querySelector(".modal-product"),sectionModal:document.querySelector(".modal"),closeBtnModal:document.querySelector(".modal__close-btn"),searchForm:document.querySelector(".search-form"),divNotFound:document.querySelector(".not-found"),currentPage:1,totalItems:0,defLimit:12};function i(e,o,r,a=!1){a&&(e.innerHTML=""),e.insertAdjacentHTML("beforeend",r(o))}const E=e=>e.map(({name:r,url:a})=>`<li class="categories__item" data-url="${a}">
+					<button  class="categories__btn" type="button">${r}
 					</button>
-				</li>`).join(""),l=t=>t.map(({id:e,title:s,description:y,dimensions:L,category:o,price:n,discountPercentage:$,rating:b,stock:f,brand:i,sku:h,reviews:C,barcode:P,images:d})=>`<li class="products__item" data-id="${e}">
-    <img class="products__image" src="${d[0]}" alt="${s}"/>
-    <p class="products__title">${s}</p>
-    <p class="products__brand"><span class="products__brand--bold">Brand:</span>${i}</p>
-    <p class="products__category">Category: ${o}</p>
-    <p class="products__price">Price: ${n}$</p>
- </li>`).join("");function g(t){t.innerHTML=""}async function c(t){return await u.get(t).then(e=>e).catch(e=>e.message)}const m=async()=>{try{const t=await c(r.qCategories);console.log(t),a(r.categoryList,t.data,p)}catch(t){console.log(t.message)}},_=async t=>{try{const e=await c(t);console.log(e),a(r.productList,e.data.products,l)}catch(e){console.log(e.message)}};r.categoryList.addEventListener("click",t=>{const e=t.target.closest(".categories__item");e&&(g(r.productList),_(e.dataset.url))});m();
+				</li>`).join("")+`<li class="categories__item" data-url="${t.BASE_URL}">
+					<button  class="categories__btn" type="button">ALL
+					</button>
+				</li>`,S=e=>e.map(({id:r,title:a,description:s,dimensions:c,category:m,price:p,discountPercentage:b,rating:h,stock:v,brand:n,sku:_,reviews:g,barcode:y,images:d})=>`<li class="products__item" data-id="${r}">
+    <img class="products__image" src="${d[0]}" alt="${a}"/>
+    <p class="products__title">${a}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand:</span>${n}</p>
+    <p class="products__category">Category: ${m}</p>
+    <p class="products__price">Price: ${p}$</p>
+ </li>`).join(""),q=({title:e,description:o,dimensions:r,category:a,price:s,discountPercentage:c,rating:m,stock:p,brand:b,sku:h,reviews:v,returnPolicy:n,images:_,shippingInformation:g,tags:y})=>{const d=y.map(P=>`<li class="modal-product__tag-item">#${P}</li>`).join(""),k=c>5?` <p class="modal-product__price-before">Price: <span class="modal-product__price-before-cross">${Math.round(s*(1+c/100))}$</span></p>`:"",f=`<img class="modal-product__img" src="${_[0]}" alt="${e}" />
+      <div class="modal-product__content">
+        <p class="modal-product__title">${e}</p>
+        <ul class="modal-product__tags">${d}</ul>
+        <p class="modal-product__description">${o}</p>
+        <p class="modal-product__shipping-information">Shipping: ${g}</p>
+        <p class="modal-product__return-policy">Return Policy: ${n}</p>${k}
+				<p class="modal-product__price">Price: ${s}$</p>
+        <button class="load-more__btn" type="button">Buy</button>
+      </div>`;return console.log(f),f};function Q(e){e.innerHTML=""}function L(e,o){e.classList.add(o)}function $(e,o){e.classList.remove(o)}async function l(e){return await M.get(e).then(o=>o).catch(o=>o.message)}const w=async()=>{try{const e=await l(t.qCategories);console.log(e),i(t.categoryList,e.data,E,!0)}catch(e){console.log(e.message)}},u=async(e,o=!1)=>{L(t.load_more,"hidden"),$(t.divNotFound,"not-found--visible");try{const r=await l(e);r.data.products.length===0&&L(t.divNotFound,"not-found--visible"),i(t.productList,r.data.products,S,!o),t.totalItems=r.data.total,console.log(t.totalItems,t.currentPage*t.defLimit),t.totalItems>t.currentPage*t.defLimit&&$(t.load_more,"hidden")}catch(r){console.log(r.message)}finally{t.load_more.disabled=!1}},B=async e=>{try{const o=await l(e);console.log(o.data,t.productModal),i(t.productModal,o.data,q,!0),t.sectionModal.classList.add("modal--is-open")}catch(o){console.log(o.message)}};t.categoryList.addEventListener("click",e=>{const o=e.target.closest(".categories__item");if(!o)return;t.currentPage=1,Q(t.productList),t.currentQuery=o.dataset.url;const r=t.currentQuery+`?limit=12&skip=${(t.currentPage-1)*12}`;u(r)});t.load_more.addEventListener("click",e=>{t.load_more.disabled=!0,t.currentPage++;const o=t.currentQuery+`&limit=${t.defLimit}&skip=${(t.currentPage-1)*t.defLimit}`;console.log(o),u(o,!0)});t.productList.addEventListener("click",e=>{const o=e.target.closest(".products__item");if(!o)return;console.log(o);const r=o.dataset.id,a=t.BASE_URL+`/${r}`;console.log(a),B(a)});t.closeBtnModal.addEventListener("click",e=>{t.sectionModal.classList.remove("modal--is-open")});t.searchForm.addEventListener("submit",e=>{e.preventDefault(),t.currentPage=1;const o=e.currentTarget.elements.searchValue.value.trim();if(!o||o.length<1)return;t.currentQuery=t.BASE_URL+`/search?q=${o}`;const r=`${t.currentQuery}&limit=${t.defLimit}&skip=${(t.currentPage-1)*t.defLimit}`;console.log(r),u(r)});w();
 //# sourceMappingURL=index.js.map
